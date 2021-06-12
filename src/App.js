b3184f78417e4e9bf6e5d2f5007690aa936e6254
Muhammad Arslan Akhtar <arslanakhtar61@gmail.com>
2021-06-12 14:29:24 -0400

    add ra-data-json-server

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Admin, Resource, ListGuesser } from 'react-admin';
-import dataProvider from './api/DataProvider';
+//import dataProvider from './api/DataProvider';
+import jsonServerProvider from 'ra-data-json-server';
 
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => {
   return (
     <Admin 
       dataProvider={dataProvider}
     >
-        <Resource name="accounts" list={ListGuesser} />
-        <Resource name="transactions" list={ListGuesser} />
+        <Resource name="users" list={ListGuesser} />
+        <Resource name="posts" list={ListGuesser} />
+        <Resource name="todos" list={ListGuesser} />
     </Admin>
   );
 }
