2566b7d2ee5d8125459acd11f0eebbdcadf77ef1
Muhammad Arslan Akhtar <arslanakhtar61@gmail.com>
2021-06-13 15:26:19 -0400

    add custom VendorList

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {fetchUtils, Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import simpleRestProvider from 'ra-data-simple-rest';
 import Dashboard from './Dashboard'
+import {VendorList} from './resources/VendorList'
 
 
 const fetchJson = (url, options = {}) => {
@@ -23,7 +24,7 @@ const App = () => {
       dataProvider={dataProvider}
       dashboard={Dashboard}
     >
-        <Resource name="vendors" options={{ label: 'Vendors' }} list={ListGuesser} />
+        <Resource name="vendors" options={{ label: 'Vendors' }} list={VendorList} />
         {/* <Resource name="posts" list={ListGuesser} />
         <Resource name="todos" list={ListGuesser} /> */}
     </Admin>
