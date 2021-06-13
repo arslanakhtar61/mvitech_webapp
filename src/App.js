10fbf6b21093451d2a8b6813e85f82ecfe40404c
Muhammad Arslan Akhtar <arslanakhtar61@gmail.com>
2021-06-13 13:28:48 -0400

    add proxy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import {fetchUtils, Admin, Resource, ListGuesser } from 'react-admin';
 //import dataProvider from './api/DataProvider';
 import jsonServerProvider from 'ra-data-json-server';
 import simpleRestProvider from 'ra-data-simple-rest';
 
+
+const fetchJson = (url, options = {}) => {
+  if (!options.headers) {
+      options.headers = new Headers({ Accept: 'application/json' });
+  }
+  // add your own headers here
+  options.headers.set('X-Custom-Header', 'foobar');
+  return fetchUtils.fetchJson(url, options);
+}
+
 //const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const dataProvider = simpleRestProvider('http://localhost:8081/');
+const dataProvider = simpleRestProvider('http://localhost:8081', fetchJson);
 const App = () => {
   return (
     <Admin 
