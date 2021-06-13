4253721127ddbfe1a5f0f11f8facce4126c2b555
Muhammad Arslan Akhtar <arslanakhtar61@gmail.com>
2021-06-13 05:42:14 -0400

    add vendors get rest endpoint

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Admin, Resource, ListGuesser } from 'react-admin';
 //import dataProvider from './api/DataProvider';
 import jsonServerProvider from 'ra-data-json-server';
+import simpleRestProvider from 'ra-data-simple-rest';
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+//const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const dataProvider = simpleRestProvider('http://localhost:8081/');
 const App = () => {
   return (
     <Admin 
       dataProvider={dataProvider}
     >
-        <Resource name="users" list={ListGuesser} />
-        <Resource name="posts" list={ListGuesser} />
-        <Resource name="todos" list={ListGuesser} />
+        <Resource name="vendors" list={ListGuesser} />
+        {/* <Resource name="posts" list={ListGuesser} />
+        <Resource name="todos" list={ListGuesser} /> */}
     </Admin>
   );
 }
